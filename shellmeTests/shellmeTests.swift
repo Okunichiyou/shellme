728e1795b635f9e28ecb74577d4bc71eb8193191
Okunichiyou <173314355+Okunichiyou@users.noreply.github.com>
2025-01-19 14:50:34 +0900

    Initial Commit

---
+++ shellmeTests/shellmeTests.swift
@@ -0,0 +1,17 @@
+//
+//  shellmeTests.swift
+//  shellmeTests
+//
+//  Created by 斉藤祐大 on 2025/01/19.
+//
+
+import Testing
+@testable import shellme
+
+struct shellmeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
